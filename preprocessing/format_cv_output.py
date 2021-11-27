106280740101cea2dde04c42673803aefcad00d6
Jack Harrison <jackharrison111@gmail.com>
2021-11-27 22:13:49 +0100

    Updating postprocessing function

--- preprocessing/format_cv_output.py
+++ preprocessing/format_cv_output.py
@@ -1,143 +1,159 @@
-
-
 import json 
 import pandas as pd 
+import numpy as np
+import gensim.parsing.preprocessing as pp
+from nltk.stem import WordNetLemmatizer 
+# # Download Wordnet through NLTK in python console:
+import nltk
+nltk.download('wordnet')
 
 
-json_file = "data/enriched_instagram.com_collector_20211106_161542..json"
-
-with open(json_file, 'r', encoding='utf-8') as file:
-    example = json.load(file)
-
-test_examples = example[:50]
-
-
-
-useful_instagram_keys = [
-'account',
-#'caption',
-'profile_name',     #This will need encoding to an ID (will help with plotting from the same user) or removing
-#'biography',
-'id',
-'following',
-'likes',
-'posts_count',
-'followers',
-'is_verified',
-'datetime',
-'comments',
-'url',                  #Useful for checking the results by eye but not for training on
-'video_url',            #Suggest encoding this into an image or video bool 
-'video_view_count',        
-'timestamp']
-#print(test_example.keys())
-
-
-all_column_headings = []
-all_flat_outputs = []
-
-for test_example in test_examples:
-
-
-    for key, val in test_example['cvResult'].items():
-        #print(key , ' -- ', val)
-        ...
-    #print('\n ---------- \n')
-
-    column_headings = []
-    flat_output = []
-
-    for key, val in test_example['instagram'].items():
-        if key in useful_instagram_keys:
-            flat_output.append(val)
-            column_headings.append(key)
+infile = "data/enriched_instagram.com_collector_20211106_161542..json"
+infiles = [
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-1",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-10",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-100",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-101",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-102",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-103",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-104",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-105",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-106",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-107",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-108",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-109",
+"enriched_BIGGER_instagram.com_collector_20211102_221942.success-11",
+"enriched_instagram.com_collector_20211106_161542."
+]
+outfile = 'data/postprocessed_data_2711.csv'
 
 
 
-    confidence_thresh = 0.3
-    full_description = []
-    for key, val in test_example['cvResult'].items():
 
-        if key == 'imageType':
-            for type, bool in val.items():
-                column_headings.append(type)
-                flat_output.append(bool)
+def process_CV_result(infile, confidence_thresh=0.5, drop_duplicates=True, lemmatizer=None, split_spaces=True, 
+                        lowercase=True, remove_stops=True, remove_punctuation=True):
 
-        if key == 'color':
-            for color_cat, color in val.items():
-                if color_cat == 'isBWImg':
-                    column_headings.append(color_cat)
-                    flat_output.append(color)
-                if color_cat == 'dominantColors':   #Could we pass these to a color embedding ? 
-                    for c in color:
-                        full_description.append(c)
+    with open(infile, 'r', encoding='utf-8') as file:
+        cv_result = json.load(file)
 
-        if key == 'adult':
-            for adult_cat, score in val.items():
-                if adult_cat in ['adultScore', 'racyScore', 'goreScore']:
-                    column_headings.append(adult_cat)
-                    flat_output.append(score)
+    all_column_headings = []
+    all_flat_outputs = []
 
-        if key == 'tags':
-            for tag in val:
-                if tag['confidence'] > confidence_thresh:
-                    full_description.append(tag['name'])
-        
-        if key == 'description':
-            [full_description.append(word) for word in val['tags']]
-            for sentence in val['captions']:
-                if sentence['confidence'] > confidence_thresh:
-                    words = sentence['text'].split(' ')
+    useful_instagram_keys = ['account','profile_name','id','following','likes','posts_count','followers',
+                            'is_verified', 'datetime','comments','url','video_url','video_view_count','timestamp']
+    #'caption' ?
+
+    for post in cv_result:
+
+        column_headings = []
+        flat_output = []
+
+        for key, val in post['instagram'].items():
+            if key in useful_instagram_keys:
+                flat_output.append(val)
+                column_headings.append(key)
+
+        full_description = []
+        for key, val in post['cvResult'].items():
+
+            if key == 'imageType':
+                for type, bool in val.items():
+                    column_headings.append(type)
+                    flat_output.append(bool)
+
+            if key == 'color':
+                for color_cat, color in val.items():
+                    if color_cat == 'isBWImg':
+                        column_headings.append(color_cat)
+                        flat_output.append(color)
+                    if color_cat == 'dominantColors':   #Could we pass these to a color embedding ? 
+                        for c in color:
+                            full_description.append(c)
+
+            if key == 'adult':
+                for adult_cat, score in val.items():
+                    if adult_cat in ['adultScore', 'racyScore', 'goreScore']:
+                        column_headings.append(adult_cat)
+                        flat_output.append(score)
+
+            if key == 'tags':
+                for tag in val:
+                    if tag['confidence'] > confidence_thresh:
+                        full_description.append(tag['name'])
+            
+            if key == 'description':
+                [full_description.append(word) for word in val['tags']]
+                all_confidences = [sentence['confidence'] for sentence in val['captions']]
+                #Find the most confident caption
+                caption = val['captions'][np.argmax(all_confidences)]
+                #Only append it if it's greater than the confidence threshold
+                if caption['confidence'] > confidence_thresh:
+                    words = caption['text'].split(' ')
                     [full_description.append(word) for word in words]
 
-        if key == 'objects':
-            for object in val:
-                if object['confidence'] > confidence_thresh:
-                    full_description.append(object['object'])
+            if key == 'objects':
+                for object in val:
+                    if object['confidence'] > confidence_thresh:
+                        full_description.append(object['object'])
 
-    column_headings.append('tokens')
-    flat_output.append(full_description)
+            #TODO: Not implemented faces, brands or categories yet
 
-    all_column_headings.append(column_headings)
-    all_flat_outputs.append(flat_output)
+        #Postprocess the individual words using NLP techniques:
+        #The order that these occur might be important, worth checking if there's time
 
-    for heading, value in zip(column_headings, flat_output):
-        #print(heading, " : " , value)
-        ...
+        #Split words
+        if split_spaces:
+            temp_desc = []
+            for word in full_description:
+                [temp_desc.append(s) for s in word.split(' ')]
+            full_description = temp_desc
+        
+        #Lowercase
+        if lowercase:
+            full_description = [word.lower() for word in full_description]
+        
+        #Stop words
+        if remove_stops:
+            #Remove stop words
+            custom_stopwords = ['a', 'of', 'to', 'next', 'and']
+            my_stop_words = pp.STOPWORDS.union(set(custom_stopwords))
+            full_description = pp.remove_stopword_tokens(full_description, stopwords=my_stop_words)
 
-#print(test_example['instagram']['url'])
+        #Lemmatize
+        if lemmatizer is not None:
+            full_description = [lemmatizer.lemmatize(word) for word in full_description]
 
-test = pd.DataFrame(all_flat_outputs, columns=all_column_headings[0])
-test.to_csv('data/test_postprocessed_data.csv')
-print(test.head())
+        #Remove apostrophes (acts as a double check on the lemmatizer as WordNet sucks)
+        if remove_punctuation:
+            full_description = [word.split("'")[0] for word in full_description]
+            full_description = [word.replace(",", "") for word in full_description]
+
+        #Drop duplicates
+        if drop_duplicates:
+            full_description = list(dict.fromkeys(full_description))
 
 
-easy_cvResult_keys = [
-    'imageType',
-    'adult',
-]
+        column_headings.append('tokens')
+        flat_output.append(full_description)
+        all_column_headings.append(column_headings)
+        all_flat_outputs.append(flat_output)
 
-useful_cvResult_keys = [
-'categories',
-'adult',
-'color',
-'imageType',
-'tags',
-'description',
-'faces',
-'objects',
-'brands'
-]
 
-''' #Take the ones that can be flattened directly and combine the others into a list of tokens using a lower threshold
-flatten_mapping = {
-    'categories' : '',
-'adult' : ['All'],   #Want to flatten to use scores?
-'color' : ['All'], #Take the dominant color and use words
-'imageType' : ['All'], #Flatten all as flags
-'tags' ,
-'description',
-'faces',
-'objects',
-'brands'}
-'''
+    output_df = pd.DataFrame(all_flat_outputs, columns=all_column_headings[0])
+    return output_df
+
+
+output_df = None
+lemmatizer = WordNetLemmatizer()
+for file in infiles:
+    input = 'data/' + file + '.json'
+
+    output = process_CV_result(input, lemmatizer=lemmatizer)
+
+    if output_df is None:
+        output_df = output
+    else:
+        output_df = pd.concat([output_df, output], axis=0)
+        
+output_df.to_csv(outfile)
+
--- scripts/word2Vec.py
+++ scripts/word2Vec.py
@@ -1,4 +1,51 @@
 import gensim
+import time
+import pandas as pd
+import ast
+import numpy as np
 
+import gensim.downloader
+from gensim.parsing.preprocessing import remove_stopwords
+
+
+# Show all available models in gensim-data
+print(list(gensim.downloader.info()['models'].keys()))
+
+start = time.perf_counter()
+print("Loading model...")
 # Load Google's pre-trained Word2Vec model.
-model = gensim.models.Word2Vec.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)
+#model = gensim.models.KeyedVectors.load_word2vec_format('./data/GoogleNews-vectors-negative300.bin.gz', binary=True)
+model = gensim.downloader.load('word2vec-google-news-300')
+#glove_vectors = gensim.downloader.load('glove-twitter-25')
+end = time.perf_counter()
+print(f"Model loaded. \nTime taken: {end-start} second(s).")
+
+
+data = pd.read_csv('./data/test_postprocessed_data.csv')
+data['token_list'] = data['tokens'].apply(lambda x: ast.literal_eval(x))
+#print(data.head())
+dataset = []
+pad_length = 100
+token_list = data['token_list']
+for test in token_list:
+    embedding_list = []
+    
+    test = remove_stopwords(test)
+    for word in test:
+        try:
+            vec = model[word]
+            embedding_list.append(model[word])
+        except:
+            print(word, " is not in the corpus.")
+    while len(embedding_list) < pad_length:
+        embedding_list.append(np.zeros((300,)))
+    embedding_list = np.array(embedding_list)
+    #print(embedding_list)
+    print(embedding_list.shape)
+    dataset.append(embedding_list)
+    emb_time = time.perf_counter()
+    
+print(emb_time - end, f" seconds taken to embed {len(dataset)} sentences.")
+dataset = np.array(dataset)
+print(dataset.shape)
+
